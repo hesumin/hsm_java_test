902361c252dedbec7a5693679763911b1529e9e4
hesumin <2581665906@qq.com>
2025-04-22 11:27:30 +0800

    Java 存储/获取 Redis 字符串( String )

---
+++ src/main/java/com/hsm/test/javaEE/redis/JavaRedisString.java
@@ -0,0 +1,28 @@
+package com.hsm.test.javaEE.redis;
+
+import redis.clients.jedis.Jedis;
+
+/**
+ * @projectName(项目名称): hsm_java_test
+ * @package(包名称): com.hsm.test.javaEE.redis
+ * @className:(类名称): JavaRedisString
+ * @author: Hesumin
+ * @createDate: 2025/04/22 11:21
+ * @description:
+ */
+
+public class JavaRedisString {
+    public static void main(String[] args) {
+        //连接本地的 Redis 服务
+        Jedis jedis = new Jedis("localhost");
+        System.out.println("连接成功");
+
+        //设置 redis 字符串数据
+        jedis.set("site", "ddkk.com");
+
+        // 获取存储的数据并输出
+        System.out.println("redis 存储的字符串为: " + jedis.get("site"));
+
+        System.out.println("删除key:" + jedis.del("site"));
+    }
+}
