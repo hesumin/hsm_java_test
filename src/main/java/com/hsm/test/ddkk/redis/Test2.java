a98bcf68a1456ee40166dad43df5656a07478491
suminhe <2581665906@qq.com>
2025-05-14 10:46:42 +0800

    给idea添加类注释模版

---
+++ src/main/java/com/hsm/test/ddkk/redis/Test2.java
@@ -0,0 +1,15 @@
+package com.hsm.test.ddkk.redis;
+
+
+/**
+ * @author suminhe
+ * @version 1.0
+ * @date 2025/5/14 10:44
+ * @description: todo
+ */
+
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
