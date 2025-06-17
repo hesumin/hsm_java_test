c4009e2363c514dead89255f18a57e7f8450ffef
suminhe <suminhe@tencent.com>
2025-06-17 19:13:44 +0800

    通过md5生成的sign签名方式

---
+++ src/main/java/com/hsm/test/sign/Test01.java
@@ -0,0 +1,31 @@
+package com.hsm.test.sign;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+/**
+ * @author suminhe
+ * @version 1.0
+ * @date 2025/6/13 16:05
+ * @description: todo
+ */
+
+public class Test01 {
+    public static void main(String[] args) {
+        String url = "http://fitdev.woa.com/permission";
+        String uri = "/sa/api/queryApprovalInfoFromWedata.do";
+        StringBuilder sb = new StringBuilder(url + uri);
+        long timestamp = System.currentTimeMillis() / 1000;
+        if (!uri.startsWith("/api/")) {
+            uri = uri.substring(uri.indexOf("/api/"));
+        }
+        String appId = "15";
+        String appToken = "5cd443da1aed45fd8b388ad82b307f1b";
+        appId = "702";
+        appToken = "d4355aaad2bd4929aff7f8b702bbb8ca";
+        // md5(appId + appToken + timestamp + api + jsonStr); jsonStr为参数
+        String sign = DigestUtils.md5Hex(appId + appToken + timestamp + uri + "{\"processId\":60215,\"processName\":\"wedata_develop_task_deploy_workflow\",\"taskName\":\"huJiaSyncTaskApproval\",\"platformName\":\"pfdd_platform_perssmion\",\"taskId\":205132}");
+        sb.append("?appId=").append(appId).append("&timestamp=").append(timestamp).append("&sign=")
+                .append(sign);
+        System.out.println(sb);
+    }
+}
